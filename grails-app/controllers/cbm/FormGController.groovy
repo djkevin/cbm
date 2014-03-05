7a5f0a12496b895758fa25fe67045ea408bc99a2
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-03-05 09:54:22 +0000

    secured controller
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@120 5eafb63e-3ce9-0410-9386-45365aa604ab

--- grails-app/controllers/cbm/FormGController.groovy
+++ grails-app/controllers/cbm/FormGController.groovy
@@ -1,10 +1,9 @@
 package cbm
 
-
+import grails.plugin.springsecurity.annotation.Secured
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
-import grails.plugin.springsecurity.annotation.Secured
 
 @Secured(['ROLE_USER'])
 @Transactional(readOnly = true)
