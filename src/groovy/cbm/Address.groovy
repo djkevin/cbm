9537251e7638f8064d30162c91458a6ed58fd2f7
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-03-05 09:54:09 +0000

    added street 1 and street 2 and state fields
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@117 5eafb63e-3ce9-0410-9386-45365aa604ab

--- src/groovy/cbm/Address.groovy
+++ src/groovy/cbm/Address.groovy
@@ -2,19 +2,23 @@ package cbm
 
 class Address {
 	
-	String street
+	String street1
+    String street2
 	String town
+    String state
 	String postCode
 	Country country
 
     static constraints = {
-		street maxSize:200
-		town maxSize:100
-		postCode maxSize:10
+		street1 maxSize:200 ,nullable: true
+		street2 maxSize:200 ,nullable: true
+		town maxSize:100 ,nullable: true
+		state maxSize:100 ,nullable: true
+		postCode maxSize:10 ,nullable: true
 		country blank:false
     }
 	
 	String toString(){
-		"${street} ${town} ${postCode} ${country}"
+		"${street1} ${street2} ${town} ${state} ${postCode} ${country}"
 	}
 }
