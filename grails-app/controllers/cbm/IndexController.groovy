ced1c755a1626b4994a7e4d4c82f8c37682e8d53
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-03-21 10:37:43 +0000

    admin login is different from SP user login
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@165 5eafb63e-3ce9-0410-9386-45365aa604ab

--- grails-app/controllers/cbm/IndexController.groovy
+++ grails-app/controllers/cbm/IndexController.groovy
@@ -1,11 +1,22 @@
 package cbm
 
+import grails.plugin.springsecurity.SpringSecurityUtils
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(['ROLE_USER'])
-class IndexController {
+@Secured(['ROLE_USER', 'ROLE_ADMIN'])
+class   IndexController {
 
     def index() {
-        redirect controller: "report", action:"index"
+        if (SpringSecurityUtils.ifAllGranted('ROLE_ADMIN')) {
+            println "redirecting to admin"
+            redirect controller: "stateParty", action:"index"
+        }
+
+        if (SpringSecurityUtils.ifAllGranted('ROLE_USER')) {
+            println "redirecting to report page"
+            redirect controller: "report", action:"index"
+        }
+
+
     }
 }
