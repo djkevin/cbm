fcbb89a76497169950b4534c79b0c31b3c81429d
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-03-26 15:41:23 +0000

    updated datasource for test env
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@198 5eafb63e-3ce9-0410-9386-45365aa604ab

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,7 +29,7 @@ environments {
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://icts-sdu-jdev:3306/testCBM"
         }
     }
     production {
