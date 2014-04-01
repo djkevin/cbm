5fd23d30af729183629a19ffffc67e61b530b8ef
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-04-01 09:50:49 +0000

    set back h2 db for dev
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@212 5eafb63e-3ce9-0410-9386-45365aa604ab

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -22,8 +22,8 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            url = "jdbc:mysql://icts-sdu-jdev:3306/testCBM"
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+          //  url = "jdbc:mysql://icts-sdu-jdev:3306/testCBM"
         }
     }
     test {
