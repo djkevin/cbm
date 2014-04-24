b3ec0fd8cf9afabbebfd889da38ee02390b38e25
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-04-24 08:19:35 +0000

    Added Country Domain class with country loader bootstrap
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@253 5eafb63e-3ce9-0410-9386-45365aa604ab

---
+++ grails-app/conf/CountryBootStrap.groovy
@@ -0,0 +1,72 @@
+import cbm.admin.Country
+import org.apache.commons.lang.StringUtils
+
+class CountryBootStrap {
+
+
+    def init = { servletContext ->
+        log.debug "Running init CountryBootStrap"
+        if (!cbm.admin.Country.count()) {
+            log.debug(" no countries in db currently")
+            String inputFile = "D:\\temp\\EFSRCA.txt"
+
+            File f = new File(inputFile)
+
+            def lineNum = 0
+            f.eachLine {
+                if(lineNum>0){
+
+                    def origLine = it.toString()
+                    //Cleanup articles, special chars
+                    def cleanedLine = it
+                            .replaceAll(~/\[masc\.\]/, '')
+                            .replaceAll(~/\[fém\.\]/, '')
+                            .replaceAll(~/\(the\)/, '')
+                            .replaceAll(~/\(le\)/, '')
+                            .replaceAll(~/\(la\)/, '')
+                            .replaceAll(~/\(les\)/, '')
+                            .replaceAll(~/\(los\)/, '')
+                            .replaceAll(~/\(las\)/, '')
+                            .replaceAll(~/\(el\)/, '')
+                            .replaceAll(~/\(l'\)/, '')
+                            .replaceAll(~/\*/,'')
+
+                    def cols = cleanedLine.split(',')
+
+                    if (cols.size()!=14){
+                        println "orig: "+origLine
+                        println "clean: " + cleanedLine
+                        println "---------------------------------------------------------------------------------------------------------------"
+                    }
+
+                    def country = new Country(
+                            iso2: cols[0].trim(),
+                            iso3: cols[1].trim(),
+                            shortNameEn: cols[2].trim(),
+                            shortNameFr: StringUtils.capitalize(cols[3].trim()),
+                            shortNameEs: StringUtils.capitalize(cols[4].trim()),
+                            shortNameRu: cols[5].trim(),
+                            shortNameCh: cols[6].trim(),
+                            shortNameAr: cols[7].trim(),
+                            formalNameEn: StringUtils.capitalize(cols[8].trim()),
+                            formalNameFr: StringUtils.capitalize(cols[9].trim()),
+                            formalNameEs: StringUtils.capitalize(cols[10].trim()),
+                            formalNameRu: cols[11].trim(),
+                            formalNameCh: cols[12].trim(),
+                            formalNameAr: cols[13].trim()
+                    ).save(flush: true)
+                   /* println "---------------------------------------------------------------------------------------------------------------"*/
+                }
+
+                lineNum++
+            }
+
+        }
+
+    }
+
+    def destroy = {
+        log.debug "Running destroy CountryBootStrap"
+
+    }
+}
---
+++ grails-app/domain/cbm/admin/Country.groovy
@@ -0,0 +1,46 @@
+package cbm.admin
+
+class Country {
+
+    String iso2
+    String iso3
+
+    String shortNameEn
+    String shortNameFr
+    String shortNameEs
+    String shortNameRu
+    String shortNameCh
+    String shortNameAr
+
+    String formalNameEn
+    String formalNameFr
+    String formalNameEs
+    String formalNameRu
+    String formalNameCh
+    String formalNameAr
+
+
+    static constraints = {
+        iso2 size:2..2, blank:false, unique:true
+        iso3 size:3..3, blank:false, unique:true
+
+        //From the ISO Country List, maximum chars short/formal = 62
+        shortNameEn blank: false, maxSize: 100
+        shortNameFr blank: false, maxSize: 100
+        shortNameEs blank: false, maxSize: 100
+        shortNameRu blank: false, maxSize: 100
+        shortNameCh blank: false, maxSize: 100
+        shortNameAr blank: false, maxSize: 100
+        
+        formalNameEn blank: false, maxSize: 250
+        formalNameFr blank: false, maxSize: 250
+        formalNameEs blank: false, maxSize: 250
+        formalNameRu blank: false, maxSize: 250
+        formalNameCh blank: false, maxSize: 250
+        formalNameAr blank: false, maxSize: 250
+    }
+
+    String toString() {
+        return "${iso2}_${iso3}_${shortNameFr}"
+    }
+}
