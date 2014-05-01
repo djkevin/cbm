fc3bbee968d52929d9793d8b923d1566415a9e2f
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-05-01 13:06:54 +0000

    Reformatted
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@264 5eafb63e-3ce9-0410-9386-45365aa604ab

--- src/groovy/cbm/BaseForm.groovy
+++ src/groovy/cbm/BaseForm.groovy
@@ -1,26 +1,20 @@
 package cbm
 
-import java.util.Date;
-import java.util.jar.Attributes.Name;
-
 abstract class BaseForm {
-	
-	static auditable = true
-	
-	String title  
-	String formStatus
-	String visibility
-	Date dateCreated
-	Date lastUpdated
-	
+
+    static auditable = true
+
+    String title
+    String formStatus
+    String visibility
+    Date dateCreated
+    Date lastUpdated
 
 
+    static constraints = {
 
-	
-	static constraints = {
-
-		formStatus inList: ['Draft','Completed']
-		visibility inList: ['Public', 'Private']
-	}
+        formStatus inList: ['Draft', 'Completed']
+        visibility inList: ['Public', 'Private']
+    }
 
 }
