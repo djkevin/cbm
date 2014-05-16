080dca10c2b3cd0486b21d726c0189c88627118a
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-05-16 08:07:56 +0000

    Changed size constraints
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@293 5eafb63e-3ce9-0410-9386-45365aa604ab

--- src/groovy/cbm/Address.groovy
+++ src/groovy/cbm/Address.groovy
@@ -1,24 +1,25 @@
 package cbm
 
+
 class Address {
-	
-	String street1
+
+    String street1
     String street2
-	String town
+    String town
     String state
-	String postCode
-	Country country
+    String postCode
+    Country country
 
     static constraints = {
-		street1 maxSize:200 ,nullable: true
-		street2 maxSize:200 ,nullable: true
-		town maxSize:100 ,nullable: true
-		state maxSize:100 ,nullable: true
-		postCode maxSize:10 ,nullable: true
-		country blank:false
+        street1 maxSize: 100, nullable: true
+        street2 maxSize: 100, nullable: true
+        town maxSize: 50, nullable: true
+        state maxSize: 50, nullable: true
+        postCode maxSize: 10, nullable: true
+        country blank: false
     }
-	
-	String toString(){
-		"${street1}, ${street2}, ${town}, ${state}, ${postCode}, ${country}"
-	}
+
+    String toString() {
+        "${street1}, ${street2}, ${town}, ${state}, ${postCode}, ${country}"
+    }
 }
