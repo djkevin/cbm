5547e645c8e2fa3b4b715de7c086a46197b0dd18
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-05-16 08:10:01 +0000

    Added log messages
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@302 5eafb63e-3ce9-0410-9386-45365aa604ab

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -32,10 +32,10 @@ class BootStrap {
                 assert SecUserSecRole.count() == 2
 				 */
 
-                println "loading statePartyData..."
+       /*         println "loading statePartyData..."
                 fixtureLoader.load("statePartyData")
                 println "loading adminData..."
-                fixtureLoader.load("adminData")
+                fixtureLoader.load("adminData")*/
 
 //				fixtureLoader.load("sampleData")
 
