08955365d8773fc0051ed002c6461a60b86918cb
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-05-16 08:08:50 +0000

    Added method hasDeclaration, toString
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@297 5eafb63e-3ce9-0410-9386-45365aa604ab

--- src/groovy/cbm/DeclarationOfMeasures.groovy
+++ src/groovy/cbm/DeclarationOfMeasures.groovy
@@ -1,23 +1,32 @@
 package cbm
 
 class DeclarationOfMeasures {
-	
-	String legislation
-	String regulation
-	String otherMeasures
-	String amendedSinceLastYear
-	
-	static constraints = {
-		legislation inList: ["Yes", "No", "Unknown"]
-		regulation inList: ["Yes", "No", "Unknown"]
-		otherMeasures inList: ["Yes", "No", "Unknown"]
-		amendedSinceLastYear inList: ["Yes", "No", "Unknown"]
-	}
 
-    String toString(){
+    String legislation
+    String regulation
+    String otherMeasures
+    String amendedSinceLastYear
+
+    static transients = ['declaration']
+
+    static constraints = {
+        legislation inList: ["Yes", "No", "Unknown"]
+        regulation inList: ["Yes", "No", "Unknown"]
+        otherMeasures inList: ["Yes", "No", "Unknown"]
+        amendedSinceLastYear inList: ["Yes", "No", "Unknown"]
+    }
+
+    String toString() {
         "${legislation} ${regulation} ${otherMeasures} ${amendedSinceLastYear}"
     }
 
+    /**
+     *  Verifies whether a declaration has been made
+      * @return True if at least one Yes filled
+     */
+    boolean hasDeclaration() {
+        (legislation.equalsIgnoreCase("Yes") || regulation.equalsIgnoreCase("Yes") && otherMeasures.equalsIgnoreCase("Yes") && amendedSinceLastYear.equalsIgnoreCase("Yes"))
+    }
 
 
 }
