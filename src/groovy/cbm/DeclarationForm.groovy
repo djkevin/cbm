305ed7c9650fd79ee4a3e4686a540d89e9f4ba37
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-06-02 13:30:18 +0000

    Fine tuned validation for yearofDeclaration/NothingToDeclare combination
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@362 5eafb63e-3ce9-0410-9386-45365aa604ab

--- src/groovy/cbm/DeclarationForm.groovy
+++ src/groovy/cbm/DeclarationForm.groovy
@@ -16,7 +16,18 @@ class DeclarationForm {
 	static constraints = {
 		nothingToDeclare()
 		nothingNewToDeclare()
-        yearOfLastDeclaration  range: currentYear..currentYear-15, nullable:true
+        yearOfLastDeclaration  range: currentYear..currentYear-15, nullable:true, validator: {val, object ->
+            def err = true
+            if (!object.nothingNewToDeclare && val){
+               err='formZero.error.year.without.nothing.new'
+            }
+            if (object.nothingNewToDeclare && !val){
+                err='formZero.error.nothing.new.without.year.error'
+            }
+
+            return err
+
+        }
 	}
 
     String toString(){
