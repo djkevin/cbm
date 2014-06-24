8819fbcf05696aa69ba7bad0b58b8dcd4c73a535
tsiamitas <tsiamitas@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-06-24 13:30:47 +0000

    fixing version number to 0.9.1
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@388 5eafb63e-3ce9-0410-9386-45365aa604ab

--- test/unit/cbm/StatePartySpec.groovy
+++ test/unit/cbm/StatePartySpec.groovy
@@ -12,7 +12,7 @@ import spock.lang.Unroll
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(StateParty)
-class StatePartySpec extends AbstractConstraintSpec {
+class StatePartySpec extends AbstractConstraintsSpec {
 
     def setup() {
         mockForConstraintsTests(StateParty, [new StateParty(country: Country.AUSTRALIA)])
