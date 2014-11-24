532d3000d36934a1ebab9100d604d332ed8ae715
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-11-24 10:03:37 +0000

    Ticket 100 - Set default option to No in pull down menu
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@482 5eafb63e-3ce9-0410-9386-45365aa604ab

--- src/groovy/cbm/DeclarationOfMeasures.groovy
+++ src/groovy/cbm/DeclarationOfMeasures.groovy
@@ -11,10 +11,10 @@ class DeclarationOfMeasures {
 
     //TODO change inLists to enum
     static constraints = {
-        legislation inList: ["Yes", "No", "Unknown"]
-        regulation inList: ["Yes", "No", "Unknown"]
-        otherMeasures inList: ["Yes", "No", "Unknown"]
-        amendedSinceLastYear inList: ["Yes", "No", "Unknown"]
+        legislation inList: ["No", "Yes", "Unknown"]
+        regulation inList: ["No", "Yes", "Unknown"]
+        otherMeasures inList: ["No", "Yes", "Unknown"]
+        amendedSinceLastYear inList: ["No", "Yes", "Unknown"]
     }
 
     String toString() {
@@ -23,7 +23,7 @@ class DeclarationOfMeasures {
 
     /**
      *  Verifies whether a declaration has been made
-      * @return True if at least one Yes filled
+     * @return True if at least one Yes filled
      */
     boolean hasDeclaration() {
         (legislation?.equalsIgnoreCase("Yes") || regulation?.equalsIgnoreCase("Yes") && otherMeasures?.equalsIgnoreCase("Yes") && amendedSinceLastYear?.equalsIgnoreCase("Yes"))
