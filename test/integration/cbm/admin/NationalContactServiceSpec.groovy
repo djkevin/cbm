21cbe6750095683e6b2cbe63c6bcd1fa8ade7cef
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-12-24 09:02:27 +0000

    added integration tests for NationalContactService
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@603 5eafb63e-3ce9-0410-9386-45365aa604ab

--- test/integration/cbm/admin/NationalContactServiceSpec.groovy
+++ test/integration/cbm/admin/NationalContactServiceSpec.groovy
@@ -6,23 +6,46 @@ class NationalContactServiceSpec extends IntegrationSpec {
 
     def nationalContactService
 
-    def setup() {
-           println "in setup"
-    }
+    def setup() {    }
 
-    def cleanup() {
-    }
+    def cleanup() {    }
 
-    void "test save national contact"() {
-        given:"A new National Contact is created"
-            def nationalContact = new NationalContact(firstName: 'a', lastName: 'a', country: Country.findByIso3('CHE'))
+    void "Test save national contact"() {
+        given: "A new National Contact is created for a StateParty"
+            def country = Country.findByIso3('CHE')
+            def nationalContact = new NationalContact(firstName: 'a', lastName: 'a', country:country, stateParty: StateParty.findByCountry(country))
 
-        when:"The domain is saved"
+        when: "The domain is saved"
             nationalContactService.save(nationalContact)
 
-        then:"A new record is created"
-           // nationalContact.id != null
-            NationalContact.count == 1
+        then: "A new nationalContact is created"
+            nationalContact.id != null
+            NationalContact.count() == 1
+
+    }
+    void "Test delete national contact"() {
+        given: "A new National Contact is created for a StateParty"
+            def country = Country.findByIso3('CHE')
+            def nationalContact = new NationalContact(firstName: 'a', lastName: 'a', country:country, stateParty: StateParty.findByCountry(country)).save()
+
+        when: "The instance is deleted"
+            nationalContactService.delete(nationalContact)
+
+        then: "The nationalContact is deleted"
+            NationalContact.count() == 0
+
+    }
+
+    void "Test delete national contact by id"() {
+        given: "A new National Contact is created for a StateParty"
+            def country = Country.findByIso3('CHE')
+            def nationalContact = new NationalContact(firstName: 'a', lastName: 'a', country:country, stateParty: StateParty.findByCountry(country)).save()
+
+        when: "The instance is deleted"
+            nationalContactService.delete(nationalContact.id)
+
+        then: "The nationalContact is deleted"
+            NationalContact.count() == 0
 
     }
 }
