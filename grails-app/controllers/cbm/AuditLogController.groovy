a7c49f17b1e8905a4a79fc125bed4e5d17272a35
jutliah <djutliah@unog.ch>
2014-12-24 22:13:28 +0100

    views for Audit

--- grails-app/controllers/cbm/AuditLogController.groovy
+++ grails-app/controllers/cbm/AuditLogController.groovy
@@ -3,11 +3,20 @@ package cbm
 import audit.AuditLog
 import grails.plugin.springsecurity.annotation.Secured
 
-@Secured(['ROLE_VIEWER', 'ROLE_EDITOR', 'ROLE_SUBMITTER'])
+@Secured(['ROLE_VIEWER', 'ROLE_EDITOR', 'ROLE_SUBMITTER', 'ROLE_ADMIN'])
 class AuditLogController {
 
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        params.sort = params.sort ?: "lastUpdated"
+        params.order = params.order ?: "desc"
+        def auditRecords = AuditLog.list(params)
+
+        respond auditRecords, model: [auditLogInstanceCount: AuditLog.count]
+    }
+
     @Secured(['ROLE_VIEWER', 'ROLE_EDITOR', 'ROLE_SUBMITTER'])
-    def loadAuditLog(Integer max){
+    def loadAuditLog(Integer max) {
         params.max = Math.min(max ?: 10, 100)
 
         def id = params.long('id')
