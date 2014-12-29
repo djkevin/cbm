4f2935db5b7123a4cdae239d29cd1cd9a0e5c25c
jutliah <jutliah@5eafb63e-3ce9-0410-9386-45365aa604ab>
2014-12-29 08:29:26 +0000

    Added unit tests for Report Service
    git-svn-id: http://svn.unog.un.org/svn/cbm/cbm/trunk@610 5eafb63e-3ce9-0410-9386-45365aa604ab

--- test/unit/cbm/ReportServiceSpec.groovy
+++ test/unit/cbm/ReportServiceSpec.groovy
@@ -31,47 +31,47 @@ class ReportServiceSpec extends Specification {
     void "Test that a Form A Part 1a with no BSL4 containment units entered without a Form A Part 1b"() {
 
         when: "A report is created with no Form A Part 1a,  no containment units and No Form A Part 1b"
-            Report report = report
+        Report report = report
 
         and: "the validation is called"
-            def error = service.validateFormAPart1BSL4(report)
+        def error = service.validateFormAPart1BSL4(report)
 
         then: "An error is thrown"
-            error != null
-            error[0] == 'error'
+        error != null
+        error[0] == 'error'
 
         when: "A report is created with a Form A Part 1a, but no containment units and No Form A Part 1b"
-             report = report
+        report = report
 
         and: "the validation is called"
-             error = service.validateFormAPart1BSL4(report)
+        error = service.validateFormAPart1BSL4(report)
 
         then: "An error is thrown"
-            error != null
-            error[0] == 'error'
+        error != null
+        error[0] == 'error'
 
         when:"A report is created with a Form A Part 1a, a BSL3 containment unit, and no Form A Part 1 b"
-            Report r = report
-            def containmentUnit = FormAPart1ContainmentUnit.build(facility: r.formAPart1[0], bioSafetyLevel: 'Enhanced BSL3')
-            r.formAPart1[0].addToFormAContainmentUnitList(containmentUnit)
+        Report r = report
+        def containmentUnit = FormAPart1ContainmentUnit.build(facility: r.formAPart1[0], bioSafetyLevel: 'Enhanced BSL3')
+        r.formAPart1[0].addToFormAContainmentUnitList(containmentUnit)
 
         and: "the validation is called"
-            error = service.validateFormAPart1BSL4(r)
+        error = service.validateFormAPart1BSL4(r)
 
         then: "An error is thrown"
-            error != null
-            error[0] == 'error'
+        error != null
+        error[0] == 'error'
 
         when:"A report is created with a Form A Part 1a, a BSL4 containment unit, and no Form A Part 1 b"
-            Report r2 = report
-            containmentUnit = FormAPart1ContainmentUnit.build(facility: r2.formAPart1[0], bioSafetyLevel: 'BSL4')
-            r2.formAPart1[0].addToFormAContainmentUnitList(containmentUnit)
+        Report r2 = report
+        containmentUnit = FormAPart1ContainmentUnit.build(facility: r2.formAPart1[0], bioSafetyLevel: 'BSL4')
+        r2.formAPart1[0].addToFormAContainmentUnitList(containmentUnit)
 
         and: "the validation is called"
-            error = service.validateFormAPart1BSL4(r)
+        error = service.validateFormAPart1BSL4(r)
 
         then: "No error is thrown"
-            error == []
+        error == []
 
     }
 
@@ -79,66 +79,52 @@ class ReportServiceSpec extends Specification {
     void "Test that a Form A Part 1a with no BSL4 containment units entered with a Form A Part 1b"() {
 
         when: "A report is created with no Form A Part 1a,  no containment units and a Form A Part 1b"
-            Report report = report
-            FormAPart1b formAPart1b = FormAPart1b.build(report: report,bioSafetyLevel2: false, bioSafetyLevel3: true)
-            report.formAPart1b = formAPart1b
+        Report report = report
+        FormAPart1b formAPart1b = FormAPart1b.build(report: report,bioSafetyLevel2: false, bioSafetyLevel3: true)
+        report.formAPart1b = formAPart1b
 
         and: "the validation is called"
-            def error = service.validateFormAPart1BSL4(report)
+        def error = service.validateFormAPart1BSL4(report)
 
         then: "No error is thrown"
-            error == []
+        error == []
     }
 
     void "Test that if Form A Part 2a is filled in, the Existing national Programmes (Form  A Part 2b) must be entered"(){
         when:"A Form A Part 2a declares it has existing programmes but no Form A Part 2b"
-            FormAPart2a formAPart2a = new FormAPart2a(existingNationalProgrammes: true, report: report)
-            report.formAPart2a = formAPart2a
-
+        FormAPart2a formAPart2a = new FormAPart2a(existingNationalProgrammes: true, report: report)
+        report.formAPart2a = formAPart2a
         and:"the validation is called"
-            def error = service.validateNationalProgrammes(report)
-
+        def error = service.validateNationalProgrammes(report)
         then: "An error is thrown"
-            error != null
-            error[0] == 'error'
-
+        error != null
+        error[0] == 'error'
         when:"A Form A Part 2a declares it has existing programmes but no Form A Part 2b"
-            formAPart2a = new FormAPart2a(existingNationalProgrammes: true, report: report)
-            report.formAPart2a = formAPart2a
-            FormAPart2b formAPart2b = FormAPart2b.build(report: report, programName: 'a', objectives: 'o')
-            report.addToFormAPart2b(formAPart2b)
-
+        formAPart2a = new FormAPart2a(existingNationalProgrammes: true, report: report)
+        report.formAPart2a = formAPart2a
+        FormAPart2b formAPart2b = FormAPart2b.build(report: report, programName: 'a', objectives: 'o')
+        report.addToFormAPart2b(formAPart2b)
         and:"the validation is called"
-            error = service.validateNationalProgrammes(report)
-
+        error = service.validateNationalProgrammes(report)
         then: "No error is thrown"
-            error == []
-
+        error == []
     }
-
     void "Test that a Report is valid when the forms are in status COMPLETED"() { //TODO test for other forms
         when:
-            Report r = report
-            r.formAPart1.clear()
-            def formAPart1a = FormAPart1a.build(report: report, country: country, location: new Address(street1: 'a'),formStatus: FormStatus.DRAFT)
-            r.formAPart1 << formAPart1a
-
+        Report r = report
+        r.formAPart1.clear()
+        def formAPart1a = FormAPart1a.build(report: report, country: country, location: new Address(street1: 'a'),formStatus: FormStatus.DRAFT)
+        r.formAPart1 << formAPart1a
         and:
-            def error = service.validateFormsCompletion(r)
-
+        def error = service.validateFormsCompletion(r)
         then: "An error is thrown"
-            error != null
-            error[0].toString().contains('error')
-
+        error != null
+        error[0].toString().contains('error')
         when:
-            formAPart1a.formStatus = FormStatus.COMPLETED
-
+        formAPart1a.formStatus = FormStatus.COMPLETED
         and:
-             error= service.validateFormsCompletion(r)
-
+        error= service.validateFormsCompletion(r)
         then: "No error is thrown"
-            error == []
+        error == []
     }
-
-
 }
